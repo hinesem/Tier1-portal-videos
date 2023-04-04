b2c2298e4b06378bf3869af431f4f310940bc398
Eric Hines <hines.eric.m@gmail.com>
2023-04-03 20:12:21 -0500

    started unit 5 videos

--- unit4and5.js
+++ unit4and5.js
@@ -1,5 +1,5 @@
 // functions are callable, repeatable code
-
+/*
 function actualMath(num0, num1){
     console.log(num0, num1);
     let answer = num0 * num1;
@@ -82,4 +82,71 @@ getInCar('hitchhiker');
 console.log('do we have enough seats?', enoughSeats());
 console.log('are we ready for the trip?', readyForRoadTrip());
 
+*/
+
 //imposter syndrome. learning from mistakes is important. seek others' input.
+
+
+// ..........  unit 5 ............
+
+/*
+think of objects as a variable with different properties. these properties can be accessed using the dot operator
+
+
+a variable holding the object ie 8 of spades
+.8
+.spades
+
+
+object.propertyName
+object[expression]
+*/
+
+//  object literals
+
+let card0 = {
+    suit: 'Hearts', // this is a property of card0
+    number: 2 // this is also a property of card0
+} // end card0
+
+let card1={
+    suit: 'Spades',
+    number: 1
+}
+function isRed(cardToCheck){ // passing an object "card0" as an argument into a function
+    console.log('in isRed', cardToCheck);
+    if(cardToCheck.suit === 'Hearts' || cardToCheck.suit === 'Diamonds'){
+        return true;
+    } // end red
+    else{
+        return false;
+    } // end !red
+} // end isRed
+
+
+
+
+
+
+console.log(isRed(card0));
+console.log(isRed(card1));
+
+let card3 = {
+    suit: 'Diamonds',
+    number: 4
+} // end card3
+
+let card4 = {
+    suit: 'Clubs',
+    number: 3
+} // end card4
+
+function isHigher(cardToCheck, cardToCheck){
+    if(cardToCheck.number > cardToCheck.number){
+    console.log(`the ${card3.number} of ${card3.suit}is greater than the ${cardToCheck.number} of ${cardToCheck.suit}.`)
+    }else{
+    console.log(`the ${cardToCheck.number} of ${cardToCheck.suit}is greater than the ${card3.number} of ${card3.suit}.`)
+    }
+}
+
+console.log(isHigher(card3, card4))
